1cdfc563243d85f501e0bbbf4672778f2eb33f1c
Cristobal Ordenes <cristobal.ordenes01@gmail.com>
2025-09-05 21:30:55 -0400

    PARTE 1 DE LA EVA 4

---
+++ bicicenter/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from  menu import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('menu/', views.display, name='menu'),
+    path('suma/', views.suma, name='suma'),
+]
