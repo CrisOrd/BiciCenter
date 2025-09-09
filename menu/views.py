6d2f54168f79e5a21045e13f18e19c581fc81d1d
Cristobal Ordenes <cristobal.ordenes01@gmail.com>
2025-09-09 00:38:35 -0300

    a

--- menu/views.py
+++ menu/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-from django.shortcuts import render
 
 def mainPage(request):
     return render(request, 'master.html')
 
-def loginPage(request):
-    return render(request, 'login.html')
+def inicioPage(request):
+    return render(request, 'inicioSesion.html')
 
 def registrarPage(request):
-    return render(request, 'registrar.html')
+    return render(request, 'registro.html')
 
 def mantemientoPage(request):
     return render(request, 'mantenimiento.html')
 
+def recuperarContraseñaPage(request):
+    return render(request, 'recuperarContraseña.html')
+
 def tienda(request):
     return render(request, 'tienda.html')
+
+
+def registroBicicleta(request):
+    return render(request, 'registrobici.html')
+
+def carrito(request):
+    return render(request, 'carrito.html')
