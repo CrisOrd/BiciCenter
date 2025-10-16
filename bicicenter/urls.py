a58e76cd59501e3a17b6c9d878480aa2e2b7d258
Cristobal Ordenes <cristobal.ordenes01@gmail.com>
2025-10-15 22:34:47 -0300

    Revert "implementacion de barra de busqueda"
    This reverts commit f72309cb4b753e8a8c9c7a92ad11f2cd46537e45.

--- bicicenter/urls.py
+++ bicicenter/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from menu import views
+from django.urls import path ,include   
+from  menu import views
 from menu.views import RegistroBicicletaView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,15 +10,14 @@ urlpatterns = [
     path('inicioSesion/', views.inicioPage, name='inicioSesion'),
     path('registro/', views.registroPage, name='registro'),
     path('logout/', views.logoutUser, name='logout'),
-    path('mantenimiento/', views.mantemientoPage, name='mantenimiento'),
+    path('mantenimiento/', views.mantemientoPage, name='mantenimiento'),  # Cambiado a mantemientoPage
     path('registrobici/', RegistroBicicletaView.as_view(), name='registrobici'),
-    path('bicicletas/', views.BicicletasListView.as_view(), name='bicicletas'),
+    path('bicicletas/', views.BicicletasListView.as_view(), name='bicicletas'), 
     path('repuestos/', views.RepuestosListView.as_view(), name='repuestos'),
     path('accesorios/', views.AccesoriosListView.as_view(), name='accesorios'),
-    path('', views.MasterListView.as_view(), name='master'),
+    path('', views.MasterListView.as_view(), name='master'), 
     path('accounts/', include('django.contrib.auth.urls')),
     path('agendar_cita/', views.agendar_cita, name='agendar_cita'),
     path('historial-mantenimientos/', views.historialMantenimientosPage, name='historial_mantenimientos'),
-    path('search/', views.search, name='search'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
